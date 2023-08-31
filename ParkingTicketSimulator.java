62942ed072bda23cac3723c43e6c09c0721024ac
Richard Brito <117929922+richardbrrito@users.noreply.github.com>
2023-08-31 17:59:16 -0400

    Update ParkingTicketSimulator.java

--- ParkingTicketSimulator.java
+++ ParkingTicketSimulator.java
@@ -2,89 +2,98 @@ import java.util.Scanner;
 public class ParkingTicketSimulator {
     public static void main(String[] args) {
         // Create a parked car object
-        ParkingTicket meter = new ParkingTicket();
-        System.out.println("Has the car paid for a ticket?");
-        String m
+        Scanner ticketScanner = new Scanner(System.in);
 
-        ParkedCar car = new ParkedCar();
-        Scanner carScanner = new Scanner(System.in);
-        System.out.println("What is the Car Make?");
-        String makeInput = carScanner.nextLine();
-        car.setMake(makeInput);
-        System.out.println("What is the car Model?");
-        String modelInput = carScanner.nextLine();
-        car.setModel(modelInput);
-        System.out.println("What is the car color?");
-        String colorInput = carScanner.nextLine();
-        car.setColor(colorInput);
-        System.out.println("What is the car year?");
-        int yearInput = carScanner.nextInt();
-        car.setYear(yearInput);
-        System.out.println("What is the License Plate?");
-        carScanner.nextLine();
-        String licenseInput = carScanner.nextLine();
-        car.setLicenseP(licenseInput);
-        System.out.println("How long has the car been parked?");
-        int minutesInput = carScanner.nextInt();
-        car.setMinutesParked(minutesInput);
+        while (true) {
+            System.out.println("Is the car over their time limit?");
+            String ticketInput = ticketScanner.nextLine();
+            if (ticketInput.equalsIgnoreCase("yes")) {
+
+                System.out.println("Ticket is being created...");
+                System.out.println(" ");
+
+                ParkedCar car = new ParkedCar();
+                Scanner carScanner = new Scanner(System.in);
+                ParkingMeter meter = new ParkingMeter();
+                ParkingTicket ticket = new ParkingTicket();
+
+                System.out.println("What is the Car Make?");
+                String makeInput = carScanner.nextLine();
+                car.setMake(makeInput);
+
+                System.out.println("What is the car Model?");
+                String modelInput = carScanner.nextLine();
+                car.setModel(modelInput);
+
+                System.out.println("What is the car color?");
+                String colorInput = carScanner.nextLine();
+                car.setColor(colorInput);
+
+                System.out.println("What is the car year?");
+                int yearInput = carScanner.nextInt();
+                car.setYear(yearInput);
+
+                System.out.println("What is the License Plate?");
+                carScanner.nextLine();
+                String licenseInput = carScanner.nextLine();
+                car.setLicenseP(licenseInput);
+
+                System.out.println("How long has the car been parked?");
+                int minutesInput = carScanner.nextInt();
+                car.setMinutesParked(minutesInput);
+                System.out.println(" ");
+
+                System.out.println("Model: " + car.getModel());
+                System.out.println("Make: " + car.getMake());
+                System.out.println("Color: " + car.getColor());
+                System.out.println("Year: " + car.getYear());
+                System.out.println("License Plate: " + car.getLicenseP());
+                System.out.println("Minutes Parked: " + car.getMinutesParked());
 
 
-        System.out.println("Model:" + car.getModel());
-        System.out.println("Make:" + car.getMake());
-        System.out.println("Color:" + car.getColor());
-        System.out.println("Year:" + car.getYear());
-        System.out.println("License Plate:" + car.getLicenseP());
-        System.out.println("Minutes Parked:" + car.getMinutesParked());
+                System.out.println(" ");
+                System.out.println("How many minutes did the car purchase?");
+                int purchasedInput = carScanner.nextInt();
+                meter.setpMinutes(purchasedInput);
+
+                PoliceOfficer policeOfficer = new PoliceOfficer();
+                Scanner policeScanner = new Scanner(System.in);
 
 
-        ParkingMeter meter = new ParkingMeter(120);
+                System.out.println(" ");
+                System.out.println("Please enter your your full name");
+                String policenameInput = policeScanner.nextLine();
+                policeOfficer.setName(policenameInput);
 
-        System.out.println(" ");
-        System.out.println("How many minutes did the car purchase?");
+                System.out.println("Please enter your badge number");
+                String policebadgeInput = policeScanner.nextLine();
+                policeOfficer.setBadge(policebadgeInput);
+                System.out.println(" ");
 
-        System.out.println(" ");
-        System.out.println("Minutes Purchased: ");
+
+                int minutesOverDue = car.getMinutesParked() - meter.getpMinutes();
+                double fine = 0.0;
+
+                if (minutesOverDue > 0) {
+                    fine = minutesOverDue * 0.50;
+                }
+
+                ticket.setFine(fine);
+
+                System.out.println("Ticket has been created.");
+                System.out.println("Police Officer: " + policeOfficer.getName());
+                System.out.println("Badge Number: " + policeOfficer.getBadge());
+                System.out.println("The total fine is: $" + String.format("%.2f", fine));
+
+
+                break;
+            } else if (ticketInput.equalsIgnoreCase("no")) {
+                System.out.println("The car is not over its limit. It does not need to be fined and registered.");
+                break;
+            } else {
+                System.out.println("Invalid input. Please enter yes or no.");
+                System.out.println(" ");
+            }
+        }
     }
 }
-
-
-
-
-//        ParkedCar car = new ParkedCar("Honda", "Civic", "Black", 2022, "ABC123", 60);
-//        System.out.println("Parked Car Info");
-//        System.out.println("Make: " + car.getMake());
-//        System.out.println("Model: " + car.getModel());
-//        System.out.println("Color: " + car.getColor());
-//        System.out.println("Year: " + car.getYear());
-//        System.out.println("License Plate: " + car.getLicenseP());
-//        System.out.println("Minutes Parked: " + car.getMinutesParked());
-//
-//        // Create a parking meter object
-//        ParkingMeter meter = new ParkingMeter(120);
-//        System.out.println("\nParking Meter Info");
-//        System.out.println("Minutes Purchased: " + meter.getpMinutes());
-//
-//        // Create a police officer object
-//        PoliceOfficer officer = new PoliceOfficer("Joseph Weathers", "12345");
-//        System.out.println("\nPolice Officer Info");
-//        System.out.println("Name: " + officer.getName());
-//        System.out.println("Badge Number: " + officer.getBadge());
-//
-//        // Check if car is parked illegally
-//        ParkingTicket ticket = officer.issueParkingTicket(car, meter);
-//        if (ticket != null) {
-//            System.out.println("\nParking Ticket Issued");
-//            System.out.println("Fine: $" + ticket.ticket());
-//            System.out.println("Reason: " + ticket.getReason());
-//            System.out.println("Officer Name: " + ticket.getOfficer().getName());
-//            System.out.println("Officer Badge Number: " + ticket.getOfficer().getBadge());
-//            System.out.println("Car Make: " + ticket.getCar().getMake());
-//            System.out.println("Car Model: " + ticket.getCar().getModel());
-//            System.out.println("Car Color: " + ticket.getCar().getColor());
-//            System.out.println("Car Year: " + ticket.getCar().getYear());
-//            System.out.println("Car License Plate: " + ticket.getCar().getLicenseP());
-//        } else {
-//            System.out.println("\nNo Parking Ticket Issued. Car is parked legally.");
-//        }
-//    }
-//}
